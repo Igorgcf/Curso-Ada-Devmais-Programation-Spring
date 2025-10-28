c5579e627a60276185c6343ff3fc76bededce62d
Igorgcf <igorTecnologia@outlook.com>
2025-10-28 15:22:01 -0300

    Resource Exception Handler

---
+++ programation-web/src/main/java/tech/ada/programation_web/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,29 @@
+package tech.ada.programation_web.resources.exceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import tech.ada.programation_web.services.exceptions.ResourceNotFoundException;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFoundException(ResourceNotFoundException e, HttpServletRequest request){
+
+        StandardError err = new StandardError();
+
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Resource not found");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+}
