85f23eed89b07c6fa04ec70269321ffe2a86e26a
Igorgcf <igorTecnologia@outlook.com>
2025-10-28 15:20:04 -0300

    Backend Project Created

---
+++ programation-web/src/main/java/tech/ada/programation_web/ProgramationWebApplication.java
@@ -0,0 +1,12 @@
+package tech.ada.programation_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramationWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramationWebApplication.class, args);
+	}
+}
---
+++ programation-web/src/test/java/tech/ada/programation_web/ProgramationWebApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ada.programation_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramationWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
