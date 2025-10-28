9538655795e03b5ecc68855de62079e6653f2480
Igorgcf <igorTecnologia@outlook.com>
2025-10-28 15:18:31 -0300

    import.sql

---
+++ programation-web/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_product (id, name, price, description, image) VALUES (UUID(), 'Smartphone', 1000.00, 'Galaxxy', 'www.image.com');
+INSERT INTO tb_product (id, name, price, description, image) VALUES (UUID(), 'TV', 2000.00, 'HD', 'www.image.com');
+INSERT INTO tb_product (id, name, price, description, image) VALUES (UUID(), 'Notebook', 3000.00, '16GB', 'www.image.com');
